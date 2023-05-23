75040911af148cb01c3911d7d35f1e86e53d669c
Abidkhan263187 <abidkhan263187@gmail.com>
2023-05-24 00:28:49 +0530

    update store and all

---
+++ furniture_store/src/Redux/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import thunk from "redux-thunk"
+import { reducer } from "./reducer";
+
+
+export const store = createStore(reducer,applyMiddleware(thunk));
