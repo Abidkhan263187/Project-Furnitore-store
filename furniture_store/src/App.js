441ffeb655e1a703151e80152b987601d7c80a71
mithunsen04 <mithunsen559@gmail.com>
2023-05-26 20:18:50 +0530

    User Info Ui

--- furniture_store/src/App.js
+++ furniture_store/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import PaymentPage from './componenets/PaymentPage';
 // import Cart from './componenets/Cart';
 
-import UserInfo from './componenets/UserInfo';
+// import UserInfo from './componenets/UserInfo';
 
 function App() {
   return (
     <div className="App">
       {/* <Cart/> */}
-   <UserInfo/>
+   {/* <UserInfo/> */}
+   <PaymentPage/>
     </div>
   );
 }
